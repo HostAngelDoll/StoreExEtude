857c970858cec126ddf16fdef4cb6683a0ebeae2
HostAngelDoll <michelahsley698@gmail.com>
2026-03-29 16:18:54 -0600

    format fix

--- core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/VideoNotesPosition.kt
+++ core/model/src/main/java/dev/anilbeesetti/nextplayer/core/model/VideoNotesPosition.kt
@@ -2,5 +2,5 @@ package dev.anilbeesetti.nextplayer.core.model
 
 enum class VideoNotesPosition {
     START, // Left in Landscape, Top in Portrait
-    END,   // Right in Landscape, Bottom in Portrait
+    END, // Right in Landscape, Bottom in Portrait
 }
